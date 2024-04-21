d7d2c5f4f518f07682bd289d9111fe64b7e4a109
Артем <iron-man1998@mail.ru>
2024-04-21 17:57:47 +0300

    Поместил всю обработку матрицы в статический класс XplusDelta

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
 
 namespace MaximumTrafficFlow
 {
@@ -22,17 +21,14 @@ namespace MaximumTrafficFlow
         private void button1_Click(object sender, EventArgs e)
         {
             Graph graph = new Graph();
-            graph.CreateListConnectedVertices();
-            List<int> path = new List<int>();
-            path = graph.BuildPath(graph.connectedVertices);
-
+            textBox1.Text = XplusDelta.StartProcess(graph.Matrix).ToString();
         }
 
         class Graph
         {
             public Graph()
             {
-                Matrix = new int[6, 6]
+                Matrix = new int[,]
                 {
                     {0,6,2,0,0,0 },
                     {4,0,8,4,0,0 },
@@ -43,24 +39,38 @@ namespace MaximumTrafficFlow
                 };
             }
             public int[,] Matrix { get; set; }
-            
-            public List<List<int>> connectedVertices = new List<List<int>>();
+        }
 
-            public void CreateListConnectedVertices()
+        static class XplusDelta
+        {
+            public static int StartProcess(int[,] matrix)
             {
+                List<List<int>> connectedVertices = new List<List<int>>();
+                List<int> path = new List<int>();
+                int minimalGraphEdge;
+                connectedVertices = CreateListConnectedVertices(matrix);
+                path = BuildPath(connectedVertices, matrix);
+                minimalGraphEdge = FindMinimumOnPath(path, matrix);
+                return minimalGraphEdge;
+            }
+
+            static List<List<int>> CreateListConnectedVertices(int[,] matrix)
+            {
+                List<List<int>> connectedVertices = new List<List<int>>();
                 List<int> usedVertices = new List<int>();
-                for (int numberRow = 0; numberRow < Matrix.GetLength(0); numberRow++)
+                for (int numberRow = 0; numberRow < matrix.GetLength(0); numberRow++)
                 {
                     List<int> rowVertices = new List<int>();
-                    for (int j = 0; j < Matrix.GetLength(1); j++)
+                    for (int j = 0; j < matrix.GetLength(1); j++)
                     {
-                        rowVertices.Add(Matrix[numberRow, j]);
+                        rowVertices.Add(matrix[numberRow, j]);
                     }
                     connectedVertices.Add(FindConnectedVerticesInRow(rowVertices, numberRow, usedVertices));
                 }
+                return connectedVertices;
             }
 
-            List<int> FindConnectedVerticesInRow(List<int> rowVertices, int numberRow, List<int> usedVertices)
+            static List<int> FindConnectedVerticesInRow(List<int> rowVertices, int numberRow, List<int> usedVertices)
             {
                 List<int> connectedVerticesInRow = new List<int>();
                 connectedVerticesInRow.Add(numberRow + 1);
@@ -75,35 +85,15 @@ namespace MaximumTrafficFlow
                 }
                 return connectedVerticesInRow;
             }
-
-            bool CheckIndividuality(int indexVertex, List<int> usedVertices)
-            {
-                foreach (var usedVertex in usedVertices)
-                {
-                    if (usedVertex == indexVertex) return false;
-                }
-                return true;
-            }
-
-            List<int> RemoveEmptyVertices(List<int> lineSegment, ref int targetVertex)
-            {
-                for (int index = lineSegment.Count - 1; index > 0; index--)
-                {
-                    if (lineSegment[index] != targetVertex && index != 0) lineSegment.RemoveAt(index);
-                }
-                if(lineSegment.Count > 1) targetVertex = lineSegment[0];
-                return lineSegment;
-            }
-
-            public List<int> BuildPath(List<List<int>> connectedVertices)
+            static List<int> BuildPath(List<List<int>> connectedVertices, int[,] matrix)
             {
                 List<int> path = new List<int>();
-                int targetVertex = Matrix.GetLength(0);
+                int targetVertex = matrix.GetLength(0);
                 List<List<int>> reverseConnectedVertices = Enumerable.Reverse(connectedVertices).ToList();
-                foreach (List<int> lineSegment in reverseConnectedVertices) 
+                foreach (List<int> lineSegment in reverseConnectedVertices)
                 {
                     RemoveEmptyVertices(lineSegment, ref targetVertex);
-                    if (lineSegment.Count > 1) 
+                    if (lineSegment.Count > 1)
                     {
                         path.Add(lineSegment[lineSegment.Count - 1]);
                         path.Add(lineSegment[0]);
@@ -112,19 +102,41 @@ namespace MaximumTrafficFlow
                 return path;
             }
 
+            static bool CheckIndividuality(int indexVertex, List<int> usedVertices)
+            {
+                foreach (var usedVertex in usedVertices)
+                {
+                    if (usedVertex == indexVertex) return false;
+                }
+                return true;
+            }
+
+            static List<int> RemoveEmptyVertices(List<int> lineSegment, ref int targetVertex)
+            {
+                for (int index = lineSegment.Count - 1; index > 0; index--)
+                {
+                    if (lineSegment[index] != targetVertex && index != 0) lineSegment.RemoveAt(index);
+                }
+                if (lineSegment.Count > 1) targetVertex = lineSegment[0];
+                return lineSegment;
+            }
+
+            static int FindMinimumOnPath(List<int> path, int[,] Matrix)
+            {
+                int minimalGraphEdge = int.MaxValue;
+                List<int> graphEdges = new List<int>();
+                for (int indexPath = 0; indexPath < path.Count; indexPath += 2)
+                {
+                    int column = path[indexPath];
+                    int row = path[indexPath + 1];
+                    graphEdges.Add(Matrix[row - 1, column - 1]);
+                }
+                foreach (var graphEdge in graphEdges)
+                {
+                    if (graphEdge < minimalGraphEdge) minimalGraphEdge = graphEdge;
+                }
+                return minimalGraphEdge;
+            }
         }
-        //void PrintMatrix(List<List<int>> matrix)
-        //{
-        //    string line = "";
-        //    foreach (List<int> list in matrix)
-        //    {
-        //        foreach (int num in list)
-        //        {
-        //            line += num.ToString() + "\t";
-        //        }
-        //        textBox1.Text += line + "\r\n";
-        //        line = "";
-        //    }
-        //}
     }
 }
