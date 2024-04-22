14d7600f918c11a6cf344447f1173a11fb708b3e
Артем <iron-man1998@mail.ru>
2024-04-22 16:31:15 +0300

    Unit тест для XplusDelta + Перемешение .git на папку выше

---
+++ MaximumTrafficFlow/Matrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaximumTrafficFlow
+{
+    public class Matrix
+    {
+        public Matrix(int[,] array)
+        {
+            Array = array;
+        }
+        public int[,] Array { get; set; }
+
+        public void PrintMatrix(TextBox textBox)
+        {
+            string row = "";
+            for (int i = 0; i < Array.GetLength(0); i++)
+            {
+                for (int j = 0; j < Array.GetLength(1); j++)
+                {
+                    row += Array[i, j].ToString() + "\t";
+                }
+                textBox.Text += row + "\r\n";
+                row = "";
+            }
+        }
+    }
+}
