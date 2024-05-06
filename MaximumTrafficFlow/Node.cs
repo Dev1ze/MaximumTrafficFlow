5f4ab0e04d3db9ae44b17544c95de56ac89fad38
Артем <iron-man1998@mail.ru>
2024-05-06 22:29:00 +0300

    Добавление вершин нажатием на ЛКМ

---
+++ MaximumTrafficFlow/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumTrafficFlow
+{
+    public class Node
+    {
+        public Node(Point positoin) 
+        {
+            Position = positoin;
+        }
+
+        public static int Radius { get; set; } = 40;
+        public Brush Color { get; set; } = Brushes.Blue;
+        public Point Position { get; set; }
+    }
+}
---
+++ MaximumTrafficFlow/VisualGraph.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaximumTrafficFlow
+{
+    public partial class VisualGraph : Form
+    {
+        public VisualGraph()
+        {
+            InitializeComponent();
+        }
+
+        public List<Node> nodes = new List<Node>();
+
+        private void VisualGraph_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                nodes.Add(new Node(e.Location));
+                Refresh();
+            }
+        }
+
+        private void VisualGraph_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var node in nodes) 
+            {
+                e.Graphics.FillEllipse(node.Color, node.Position.X - Node.Radius / 2, node.Position.Y - Node.Radius / 2, Node.Radius, Node.Radius);
+            }
+        }
+    }
+}
