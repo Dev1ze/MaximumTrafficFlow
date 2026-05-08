e33c9eee747da5723867b243818ed1a42f2fd50d
ARTEM\iron- <iron-man1998@mail.ru>
2026-05-08 22:57:23 +0300

    Исправлено формирование множества Б

--- GraphMinCutLibrary/Multitude.cs
+++ GraphMinCutLibrary/Multitude.cs
@@ -58,54 +58,162 @@ namespace GraphMinCutLibrary
 
         private static List<int> FindMultitudeB(List<List<int>> list)
         {
-            List<List<int>> reverseConnectedVertices = new List<List<int>>();
-            reverseConnectedVertices = Enumerable.Reverse(list).ToList();
-            int target = 0;
-            List<int> multitude = new List<int>();
-            foreach (var item in reverseConnectedVertices)
-            {
-                if (item.Count == 1) 
-                {
-                    multitude.Add(item[0]);
-                }
-                else if (item.Count > 1) 
-                {
-                    multitude.Add(item.Last());
-                    multitude.Add(item.First());
-                    target = item.First();
-                    if(target == 1)
-                    {
-                        List<int> multitudeB = new List<int>();
-                        List<int> multitudeA = new List<int>();
-                        multitudeA = reverseConnectedVertices.Last();
-                        for(int i = reverseConnectedVertices.First()[0]; i > 0; i--)
-                        {
-                            multitudeB.Add(i);
-                        }
-                        IEnumerable<int> difference = multitudeB.Except(multitudeA);
-                        List<int> resultList = difference.ToList();
-                        return resultList;
-                    }
-                    break;
-                }
-            }
-            foreach (var item in reverseConnectedVertices)
-            {
-                if (item.Count > 1)
-                {
-                    for(int i = item.Count - 1; i != 0; i--)
-                    {
-                        if(item[i] == target)
-                        {
-                            multitude.Add(item.Last());
-                            multitude.Add(item.First());
-                            target = item.First();
-                        }
-                    }
-                }
-            }
-            return multitude;
+            // 1. Узнаём максимальный номер вершины во всём списке списков
+            int totalCount = list
+                .SelectMany(x => x)
+                .Max();
+
+            // 2. Создаём большой список: 1, 2, 3, ..., totalCount
+            List<int> allNumbers = Enumerable.Range(1, totalCount).ToList();
+
+            // 3. Находим самый длинный путь
+            List<int> longestPath = FindLongestPath(list);
+
+            if (longestPath.Count == 0)
+                return allNumbers;
+
+            // 4. Берём последнюю вершину самого длинного пути
+            int farthestVertex = longestPath.Last();
+
+            // 5. Создаём список от 1 до самой дальней вершины
+            List<int> reachedNumbers = Enumerable.Range(1, farthestVertex).ToList();
+
+            // 6. Вычитаем reachedNumbers из allNumbers
+            HashSet<int> reachedSet = new HashSet<int>(reachedNumbers);
+
+            List<int> result = allNumbers
+                .Where(x => !reachedSet.Contains(x))
+                .ToList();
+
+            return result;
         }
+
+        private static List<int> FindLongestPath(List<List<int>> input)
+        {
+            Dictionary<int, List<int>> graph = BuildGraph(input);
+
+            List<int> bestPath = new List<int>();
+
+            foreach (int startVertex in graph.Keys)
+            {
+                List<int> currentPath = new List<int>();
+                HashSet<int> visited = new HashSet<int>();
+
+                Dfs(startVertex, graph, visited, currentPath, ref bestPath);
+            }
+
+            return bestPath;
+        }
+
+        private static void Dfs(
+            int current,
+            Dictionary<int, List<int>> graph,
+            HashSet<int> visited,
+            List<int> currentPath,
+            ref List<int> bestPath)
+        {
+            visited.Add(current);
+            currentPath.Add(current);
+
+            if (currentPath.Count > bestPath.Count)
+            {
+                bestPath = new List<int>(currentPath);
+            }
+
+            if (graph.ContainsKey(current))
+            {
+                foreach (int next in graph[current])
+                {
+                    if (!visited.Contains(next))
+                    {
+                        Dfs(next, graph, visited, currentPath, ref bestPath);
+                    }
+                }
+            }
+
+            currentPath.RemoveAt(currentPath.Count - 1);
+            visited.Remove(current);
+        }
+
+        private static Dictionary<int, List<int>> BuildGraph(List<List<int>> input)
+        {
+            Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+            foreach (List<int> row in input)
+            {
+                if (row == null || row.Count == 0)
+                    continue;
+
+                int from = row[0];
+
+                if (!graph.ContainsKey(from))
+                    graph[from] = new List<int>();
+
+                for (int i = 1; i < row.Count; i++)
+                {
+                    graph[from].Add(row[i]);
+                }
+            }
+
+            return graph;
+        }
+
+
+
+
+
+
+
+
+
+
+        //List<List<int>> reverseConnectedVertices = new List<List<int>>();
+        //reverseConnectedVertices = Enumerable.Reverse(list).ToList();
+        //int target = 0;
+        //List<int> multitude = new List<int>();
+        //foreach (var item in reverseConnectedVertices)
+        //{
+        //    if (item.Count == 1) 
+        //    {
+        //        multitude.Add(item[0]);
+        //    }
+        //    else if (item.Count > 1) 
+        //    {
+        //        multitude.Add(item.Last());
+        //        multitude.Add(item.First());
+        //        target = item.First();
+        //        if(target == 1)
+        //        {
+        //            List<int> multitudeB = new List<int>();
+        //            List<int> multitudeA = new List<int>();
+        //            multitudeA = reverseConnectedVertices.Last();
+        //            for(int i = reverseConnectedVertices.First()[0]; i > 0; i--)
+        //            {
+        //                multitudeB.Add(i);
+        //            }
+        //            IEnumerable<int> difference = multitudeB.Except(multitudeA);
+        //            List<int> resultList = difference.ToList();
+        //            return resultList;
+        //        }
+        //        break;
+        //    }
+        //}
+        //foreach (var item in reverseConnectedVertices)
+        //{
+        //    if (item.Count > 1)
+        //    {
+        //        for(int i = item.Count - 1; i != 0; i--)
+        //        {
+        //            if(item[i] == target)
+        //            {
+        //                multitude.Add(item.Last());
+        //                multitude.Add(item.First());
+        //                target = item.First();
+        //            }
+        //        }
+        //    }
+        //}
+        //return multitude;
 
         private static List<int> RemoveDuplicates(List<int> list)
         {
